7cc3bf8329a790a17996f56becceedeab8e679c9
Maghish <mail4mithran@gmail.com>
2024-02-12 23:14:58 +0530

    Added Navbar component pt-4

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 function Navbar(props) {
     return (
-        <div className="bg-[#252525] w-full min-h-[72px] flex items-center pl-9 pr-4">
+        <div className="bg-[#252525] w-full min-h-[72px] flex items-center pl-7 pr-4">
             <p className="font-mono text-white text-2xl"><Link to="/">Remind Me</Link></p>
             <p className="font-mono text-stone-300 text-md pl-14 hover:text-white"><Link>Docs</Link></p>
             <p className="font-mono text-stone-300 text-md pl-10 hover:text-white"><Link>Create a task</Link></p>
