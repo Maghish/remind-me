8e332c526f281f8dfc47b84debf7a268ca49f805
Maghish <mail4mithran@gmail.com>
2024-02-13 09:11:22 +0530

    Added Tasks display box pt-3

--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const taskSchema = new Schema({
     name: {type: String, required: true},
     description: {type: String, required: true},
-    rank: {type: String, required: true, enum: ['Most Important thing in the world', 'Very Important', 'Important', 'Just need to do it', 'Optional', 'Not required']},
+    rank: {type: Number, required: true},
 }, 
 {
     timestamps: true
