14c41dedb27f088340dcec8662a8070f3734bad0
Maghish <mail4mithran@gmail.com>
2024-02-13 09:11:29 +0530

    Added Tasks display box pt-4

--- client/src/components/TaskBox.jsx
+++ client/src/components/TaskBox.jsx
@@ -1,6 +1,6 @@
 function TaskBox(props) {
     return (
-        <div className="flex flex-col flex-shrink-0 flex-1 flex-grow-0 min-w-[220px] h-[220px] mt-5 bg-gray-400 rounded-2xl p-7">
+        <div className="flex flex-col flex-shrink-0 flex-1 flex-grow-0 min-w-[220px] h-[220px] mt-5 bg-stone-700 rounded-2xl p-7 cursor-pointer mr-11">
             <p className="font-mono text-white text-md">{props.title}</p>
             <div className="max-h-[4.5rem] overflow-auto scrollbar-hide mt-3">
                 <p className="font-mono text-white text-sm text-pre-line">{props.description}</p>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,43 @@
+import { useEffect, useState } from "react"
+
 import Navbar from "../components/Navbar"
 import TaskBox from "../components/TaskBox"
 
+import axios from "axios"
+
 function Home(props) {
+    
+    const [allTasks, setAllTasks] = useState([])
+
+    useEffect(() => {
+        async function getAllTasks() {
+            try {
+                const res = await axios.get("/api")
+                setAllTasks(res.data)
+            }
+
+            catch (error) {
+                throw error
+            }
+        }
+
+        getAllTasks()
+    }, [])
+
     return (
         <>
             <Navbar username={props.username} />
             <div className="w-full min-h-screen flex flex-col items-center">
                 <p className="font-mono text-white text-4xl pt-9">Welcome Back {props.username}!</p>
-                <div className="flex flex-wrap justify-between w-full min-h-32 h-auto px-32 pt-7 flex-shrink-0 backdrop-filter backdrop-blur-lg rounded-2xl">
-                    <TaskBox title="Do Freelance" description="Freelancing is a great way to earn money and if you reach 18 then you can be a millionare in no time!" rank="Very Important" />
-                    <TaskBox title="Learn Next.js" description="Next.js is important" rank="Important" />
-                    <TaskBox title="Learn AWS" description="Recruiters expect applicants to know AWS so it's important" rank="Important" />
-                    <TaskBox title="Complete this project" description="Yes you need to do a lot of projects but is THIS project necessary.. well it's mid you can do it if you want else you can ignore" rank="Mid" />
-                    <TaskBox title="Learn MEAN Stack" description="MEAN Stack is harder than MERN Stack but it's worth a glance" rank="Not necessary" />
+                <div className="flex flex-wrap w-full min-h-32 h-auto px-32 pt-7 flex-shrink-0 backdrop-filter backdrop-blur-lg rounded-2xl">
+                    {/* <TaskBox title="Do Freelance" description="Freelancing is a great way to earn money and if you reach 18 then you can be a millionare in no time!" rank="78" />
+                    <TaskBox title="Learn Next.js" description="Next.js is important" rank="45" />
+                    <TaskBox title="Learn AWS" description="Recruiters expect applicants to know AWS so it's important" rank="35" />
+                    <TaskBox title="Complete this project" description="Yes you need to do a lot of projects but is THIS project necessary.. well it's mid you can do it if you want else you can ignore" rank="21" />
+                    <TaskBox title="Learn MEAN Stack" description="MEAN Stack is harder than MERN Stack but it's worth a glance" rank="5" /> */}
+                    {allTasks.map(task => {
+                        return <TaskBox title={task.name} description={task.description} rank={task.rank} />
+                    })}
                 </div>
             </div>
         </>
