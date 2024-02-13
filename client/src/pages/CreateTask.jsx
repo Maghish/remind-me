897a92fde3658cef2506701d225bb897fc6a40ad
Maghish <mail4mithran@gmail.com>
2024-02-13 10:34:29 +0530

    Added Tasks display box pt-7

--- client/src/pages/CreateTask.jsx
+++ client/src/pages/CreateTask.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react"
+import axios from "axios"
+
+import GetCookie from "../hooks/GetCookie"
+
 function CreateTask() {
+    useEffect(() => {
+        async function createTask() {
+            axios.post('/auth', {token: GetCookie('userToken')})
+            .then(currentUser => {
+                axios.post('/api/createtask', {name: 'Damn345', description: 'This is damn', rank: 89, creatorID: currentUser.data.userData._id})
+                .then(res => {})
+            })
+        }
+
+        createTask();
+    }, [])
+
     return (
         <>
             <p>Create Task</p>
