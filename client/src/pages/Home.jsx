27e441c8ab2453448584fd30c025dbc1fa7b9259
Maghish <mail4mithran@gmail.com>
2024-03-08 10:54:37 +0530

    Added error message

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,65 +1,94 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-import Navbar from "../components/Navbar"
-import TaskBox from "../components/TaskBox"
+import Navbar from "../components/Navbar";
+import TaskBox from "../components/TaskBox";
 
-import GetCookie from '../hooks/GetCookie'
+import GetCookie from "../hooks/GetCookie";
 
-import axios from "axios"
+import axios from "axios";
 
-function Home(props) {
-    
-    const [allTasks, setAllTasks] = useState([])
-    const [taskName, setTaskName] = useState(null)
-    const [taskDesc, setTaskDesc] = useState(null)
-    const [taskRank, setTaskRank] = useState(null)
+function AllTasksWrap() {
+  const [allTasks, setAllTasks] = useState([]);
+  const [taskName, setTaskName] = useState(null);
+  const [taskDesc, setTaskDesc] = useState(null);
+  const [taskRank, setTaskRank] = useState(null);
 
-    useEffect(() => {
-        async function getAllTasks() {
-            try {
-                axios.post('/auth', {token: GetCookie('userToken')})
-                .then(currentUser => {
-                    const currentUserId = currentUser.data.userData._id;
-                    axios.post("/api/gettasks", {creatorID: currentUserId})
-                    .then(res => {
-                        setAllTasks(res.data)
-                        setTaskName(res.data[0].name)
-                        setTaskDesc(res.data[0].description)
-                        setTaskRank(res.data[0].rank)
-                    })
-                   
-                })
-            }
+  useEffect(() => {
+    async function getAllTasks() {
+      try {
+        axios
+          .post("/auth", { token: GetCookie("userToken") })
+          .then((currentUser) => {
+            const currentUserId = currentUser.data.userData._id;
+            axios
+              .post("/api/gettasks", { creatorID: currentUserId })
+              .then((res) => {
+                setAllTasks(res.data);
+                setTaskName(res.data[0].name);
+                setTaskDesc(res.data[0].description);
+                setTaskRank(res.data[0].rank);
+              });
+          });
+      } catch (error) {
+        throw error;
+      }
+    }
 
-            catch (error) {
-                throw error
-            }
-        }
+    getAllTasks();
+  }, []);
 
-        getAllTasks()
-    }, [])
- 
+  if (allTasks.length === 0) {
     return (
-        <>
-            <Navbar username={props.username} />
-            <div className="w-full min-h-screen flex flex-col items-center">
-                <p className="font-mono text-white text-4xl pt-9">Welcome Back {props.username}!</p>
-                <div className="flex flex-row w-full h-full mt-7">
-                    <div className="flex flex-wrap w-[70%] min-h-32 max-h-[70vh] px-32 pt-7 flex-shrink-0 backdrop-filter backdrop-blur-lg rounded-2xl overflow-auto scrollbar-hide">
-                        {allTasks.map((task, key) => { 
-                            return <TaskBox key={key} title={task.name} description={task.description} rank={task.rank} onClick={() => {setTaskName(task.name); setTaskDesc(task.description); setTaskRank(task.rank);}} />
-                        })}
-                    </div>
-                    <div className="w-[30%] min-h-32 h-auto border-l-2 border-l-stone-600 pt-7 pl-8 ">
-                        <p className="font-mono text-white text-lg mb-7">{taskName}</p>
-                        <p className="font-mono text-white text-sm mb-11">{taskDesc}</p>
-                        <p className="font-mono text-white text-md">#{taskRank}</p>
-                    </div>
-                </div>
-            </div>
-            
-        </>
-    )
+      <div className="w-full min-h-full flex items-center justify-center">
+        <div className="border-2 rounded-lg border-stone-600 p-4">
+          <span className="font-mono text-white">No tasks found</span>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <div className="flex flex-wrap w-[70%] min-h-32 max-h-[70vh] px-32 pt-7 flex-shrink-0 backdrop-filter backdrop-blur-lg rounded-2xl overflow-auto scrollbar-hide">
+          {allTasks.map((task, key) => {
+            return (
+              <TaskBox
+                key={key}
+                title={task.name}
+                description={task.description}
+                rank={task.rank}
+                onClick={() => {
+                  setTaskName(task.name);
+                  setTaskDesc(task.description);
+                  setTaskRank(task.rank);
+                }}
+              />
+            );
+          })}
+        </div>
+        <div className="w-[30%] min-h-32 h-auto border-l-2 border-l-stone-600 pt-7 pl-8 ">
+          <p className="font-mono text-white text-lg mb-7">{taskName}</p>
+          <p className="font-mono text-white text-sm mb-11">{taskDesc}</p>
+          <p className="font-mono text-white text-md">#{taskRank}</p>
+        </div>
+      </>
+    );
+  }
 }
 
-export default Home
+function Home(props) {
+  return (
+    <>
+      <Navbar username={props.username} />
+      <div className="w-full min-h-screen flex flex-col items-center">
+        <p className="font-mono text-white text-4xl pt-9">
+          Welcome Back {props.username}!
+        </p>
+        <div className="flex flex-row w-full h-full mt-7">
+          <AllTasksWrap />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
--- server/controllers/authControl.js
+++ server/controllers/authControl.js
@@ -1,143 +1,150 @@
-const authUserModel = require('../models/authUserModel');
+const authUserModel = require("../models/authUserModel");
 
 const getCurrentUser = async (req, res) => {
-    try {
-        const {token} = req.body;
-        const email = atob(token.toString());
-        const allUsers = await authUserModel.find({});
-        let t = null;
-        allUsers.forEach(user => {
-            if (user.email === email) {
-                t=1;
-                res.status(200).json({
-                    userData: user
-                });
+  try {
+    const { token } = req.body;
+    const email = atob(token);
+    const allUsers = await authUserModel.find({});
+    let t = null;
+    allUsers.forEach((user) => {
+      if (user.email === email) {
+        t = 1;
+        res.status(200).json({
+          userData: user,
+        });
 
-                return
-            }
-        })
+        return;
+      }
+    });
 
-        if (t === null) {
-            res.status(400).json({
-                errorMessage: 'Invalid Token'
-            });
+    if (t === null) {
+      res.status(400).json({
+        errorMessage: "Invalid Token",
+      });
 
-            return
-        }
+      return;
     }
+  } catch (error) {
+    res.status(400).json(error.message);
 
-    catch (error) {
-        res.status(400).json(error.message);
-
-        return
-    }
-}
+    return;
+  }
+};
 
 const createNewUser = async (req, res) => {
-    try {
-        const {username, email, password} = req.body;
-        const allUsers = await authUserModel.find({});
-        let t = null;
-        allUsers.forEach(user => {
-            if (user.username.trimEnd() === username.trimEnd()) {
-                t=1;
-                res.status(400).json({
-                    errorMessage: 'User already exists with the same username'
-                }); 
+  try {
+    const { username, email, password } = req.body;
+    const allUsers = await authUserModel.find({});
+    let t = null;
+    allUsers.forEach((user) => {
+      if (user.username.trimEnd() === username.trimEnd()) {
+        t = 1;
+        res.status(400).json({
+          errorMessage: "User already exists with the same username",
+        });
 
-                return
-            }
+        return;
+      }
 
-            if (user.email.trimEnd() === email.trimEnd()) {
-                t=1;
-                res.status(400).json({
-                    errorMessage: 'User already exists with the same email'
-                }); 
+      if (user.email.trimEnd() === email.trimEnd()) {
+        t = 1;
+        res.status(400).json({
+          errorMessage: "User already exists with the same email",
+        });
 
-                return 
-            }
-        }) 
+        return;
+      }
+    });
 
-        if (t === null) {
-            const newUser = await authUserModel.create({username, email, password});
-            res.status(200).json({
-                userData: newUser,
-                token: btoa(email)
-            });
+    if (t === null) {
+      const newUser = await authUserModel.create({ username, email, password });
+      res.status(200).json({
+        userData: newUser,
+        token: btoa(email),
+      });
 
-            return
-        }
+      return;
     }
-    
-    catch (error) {
-        res.status(400).json(error.message);
+  } catch (error) {
+    res.status(400).json(error.message);
 
-        return
-    }
-}
+    return;
+  }
+};
 
 const logInUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const allUsers = await authUserModel.find({});
+    let t = null;
+    let user;
     try {
-        const {email, password} = req.body;
-        const allUsers = await authUserModel.find({})
-        let t = null;
-        let user;
-        try {
-            allUsers.forEach(user => {
-                if (user.email.trimEnd() === email.trimEnd() && user.password.trimEnd() === password.trimEnd()) {t=1; user=user; throw new SyntaxError;}
-                if (user.email.trimEnd() === email.trimEnd() && user.password.trimEnd() !== password.trimEnd()) {t=2; user=user}
-                if (user.email.trimEnd() !== email.trimEnd()) {t=3; user=user}
-            })
+      allUsers.forEach((user) => {
+        if (
+          user.email.trimEnd() === email.trimEnd() &&
+          user.password.trimEnd() === password.trimEnd()
+        ) {
+          t = 1;
+          user = user;
+          throw new SyntaxError();
         }
-
-        catch (error) {}
-        
-
-        if (t === null) {
-            res.status(400).json({
-                errorMessage: 'Invalid Email'
-            })
-
-            return
+        if (
+          user.email.trimEnd() === email.trimEnd() &&
+          user.password.trimEnd() !== password.trimEnd()
+        ) {
+          t = 2;
+          user = user;
         }
-
-        if (t === 1) {
-            res.status(200).json({
-                userData: user,
-                token: btoa(email)
-            })
-
-            return
+        if (user.email.trimEnd() !== email.trimEnd()) {
+          t = 3;
+          user = user;
         }
+      });
+    } catch (error) {}
 
-        if (t === 2) {
-            res.status(400).json({
-                errorMessage: 'Invalid Password'                    
-            })
+    if (t === null) {
+      res.status(400).json({
+        errorMessage: "Invalid Email",
+      });
 
-            return
-        }
-
-        if (t === 3) {
-            res.status(400).json({
-                errorMessage: 'Invalid Email'
-            })
-
-            return
-        }
+      return;
     }
 
-    catch (error) {
-        res.status(400).json({
-            errorMessage: error.message
-        })
+    if (t === 1) {
+      res.status(200).json({
+        userData: user,
+        token: btoa(email),
+      });
 
-        return
+      return;
     }
-}
+
+    if (t === 2) {
+      res.status(400).json({
+        errorMessage: "Invalid Password",
+      });
+
+      return;
+    }
+
+    if (t === 3) {
+      res.status(400).json({
+        errorMessage: "Invalid Email",
+      });
+
+      return;
+    }
+  } catch (error) {
+    res.status(400).json({
+      errorMessage: error.message,
+    });
+
+    return;
+  }
+};
 
 module.exports = {
-    getCurrentUser,
-    createNewUser,
-    logInUser
-}
+  getCurrentUser,
+  createNewUser,
+  logInUser,
+};
