f4d1bd735cd15a7896d69e32d2dcd513d9f61475
Maghish <mail4mithran@gmail.com>
2024-03-27 23:19:35 +0530

    Created loginForm frontend

--- client/src/app/components/Loginform.tsx
+++ client/src/app/components/Loginform.tsx
@@ -1,7 +1,62 @@
-function Loginform() {
+import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+
+function Loginform({ setLoginFormVisibility, setSignupFormVisibility }: any) {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
   return (
-    <></>
+    <div className="absolute min-w-full min-h-full backdrop-blur-md flex items-center justify-center">
+      <form className="relative min-w-[500px] w-auto min-h-[600px] h-auto bg-black p-10 rounded-lg flex flex-col gap-y-10">
+        <IoMdClose
+          className="absolute self-end top-[25px] right-[30px] cursor-pointer"
+          size="18px"
+          color="#fafaf9"
+          onClick={() => {
+            setLoginFormVisibility(false);
+          }}
+        />
+        <span className="text-3xl font-mono font-semibold text-center text-stone-50">
+          Log in
+        </span>
+        <div className="flex flex-col gap-y-6 min-h-[300px]">
+          <input
+            className="w-full px-4 py-3 bg-stone-200 outline-none rounded-md font-mono text-black placeholder:font-mono placeholder:text-stone-400"
+            placeholder="Enter Email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          ></input>
+          <input
+            className="w-full px-4 py-3 bg-stone-200 outline-none rounded-md font-mono text-black placeholder:font-mono placeholder:text-stone-400"
+            placeholder="Enter Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+          ></input>
+        </div>
+        <button
+          type="button"
+          className="bg-inherit border-2 border-stone-100 rounded-md p-3 px-6 w-fit font-mono text-sm text-stone-100 self-center hover:opacity-90"
+        >
+          Log in
+        </button>
+        <span className="font-mono text-sm text-stone-200 text-center">
+          Don't have an account?{" "}
+          <span
+            className="text-red-500 cursor-pointer hover:underline hover:underline-offset-2"
+            onClick={() => {
+              setLoginFormVisibility(false);
+              setSignupFormVisibility(true);
+            }}
+          >
+            Sign up!
+          </span>
+        </span>
+      </form>
+    </div>
   );
 }
- 
+
 export default Loginform;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,30 @@
-export default function Home() {
+"use client";
+
+import Loginform from "./components/Loginform";
+import Signupform from "./components/Signupform";
+import { useState } from "react";
+
+function Home() {
+  const [loginFormVisible, setLoginFormVisible] = useState<boolean>(false);
+  const [signupFormVisible, setSignupFormVisible] = useState<boolean>(false);
+
   return (
-    <></>
+    <div className="flex gap-x-4">
+      <button
+        className="ml-5 min-w-11 w-auto min-h-11 py-3 px-4 border-4 border-black bg-inherit rounded-md"
+        onClick={() => {
+          setLoginFormVisible(true);
+        }}
+      >
+        Login
+      </button>
+      <button className="min-w-11 w-auto min-h-11 py-3 px-4 border-4 border-black bg-inherit rounded-md">
+        Signup
+      </button>
+      {loginFormVisible ? <Loginform setLoginFormVisibility={(v: boolean) => setLoginFormVisible(v)} setSignupFormVisibility={(v: boolean) => setSignupFormVisible(v)} /> : ""}
+      {signupFormVisible ? <Signupform /> : ""}
+    </div>
   );
 }
+
+export default Home;
