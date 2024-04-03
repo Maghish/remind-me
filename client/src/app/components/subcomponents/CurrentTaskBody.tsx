bf2078b53e22b2b13039b609e08008422a7dd395
Maghish <mail4mithran@gmail.com>
2024-04-03 12:42:46 +0530

    Made currentTask render on the CurrentTaskBody component

--- client/src/app/components/subcomponents/CurrentTaskBody.tsx
+++ client/src/app/components/subcomponents/CurrentTaskBody.tsx
@@ -1,11 +1,22 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { HoverContext } from "@/app/contexts/hoverContext";
 
 function CurrentTaskBody() {
   const { taskDetails } = useContext(HoverContext);
+  const [name, setName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [rank, setRank] = useState<number>();
+  const [state, setState] = useState<"Open" | "Closed" | "Saved">("Open");
+
+  if (taskDetails) {
+    setName(taskDetails[0]);
+    setDescription(taskDetails[1]);
+    setRank(taskDetails[2]);
+    setState(taskDetails[3]);
+  }
 
   return (
-    <p>{taskDetails} Hmm</p>
+    <p>{taskDetails ? taskDetails[0] : ""}</p>
   );
 }
  
--- client/src/app/contexts/hoverContext.tsx
+++ client/src/app/contexts/hoverContext.tsx
@@ -4,5 +4,5 @@ import { createContext, useState } from "react";
 
 export const HoverContext = createContext({
   taskDetails: null,
-  setTaskDetails: (updatedTaskDetails: any) => {},
+  setTaskDetails: (updatedTaskDetails: any[]) => {},
 });
