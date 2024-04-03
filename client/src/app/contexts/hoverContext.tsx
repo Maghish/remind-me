9f8b1dbdfd95ad8b58b2e2ef7e81c8e2adec3f37
Maghish <mail4mithran@gmail.com>
2024-04-03 12:23:05 +0530

    Created hoverContext

---
+++ client/src/app/contexts/hoverContext.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import { createContext, useState } from "react";
+
+export const HoverContext = createContext({
+  taskDetails: null,
+  setTaskDetails: (updatedTaskDetails: any) => {},
+});
