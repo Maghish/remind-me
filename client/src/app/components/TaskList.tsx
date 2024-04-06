b0824d6ebd6c014274d2d876cfe301c54eea7ed7
Maghish <mail4mithran@gmail.com>
2024-04-06 18:44:58 +0530

    Adjusted paddings, etc to make it responsive

--- client/src/app/components/TaskList.tsx
+++ client/src/app/components/TaskList.tsx
@@ -7,8 +7,8 @@ function TaskList({ allTasks }: any) {
   const [taskDetails, setTaskDetails] = useState<TaskType | null>(null);
 
   return (
-    <div className="mt-[90px] w-full h-auto p-6 flex flex-row px-40 gap-x-40">
-      <div className="flex flex-col gap-y-4 max-h-[620px] max-w-[470px] flex-grow-0 flex-shrink-0 flex-nowrap overflow-auto no-scrollbar">
+    <div className="mt-[90px] w-full h-auto py-6 px-10 lg:px-20 xl:px-40 flex flex-row justify-center gap-x-12 lg:gap-x-20 xl:gap-x-40">
+      <div className="flex flex-col gap-y-4 max-h-screen sm:max-h-[620px] max-w-[470px] w-full flex-grow-0 flex-shrink-0 flex-nowrap overflow-auto no-scrollbar">
         {allTasks.map((task: any) => {
           return (
             <TaskBody
