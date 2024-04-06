0f64e8bbc59a6904365c82ed0f4d5b0875966f37
Maghish <mail4mithran@gmail.com>
2024-04-06 19:06:34 +0530

    Adjusted EditCurrentTaskModal width on smaller screens

--- client/src/app/components/subcomponents/EditCurrentTaskModal.tsx
+++ client/src/app/components/subcomponents/EditCurrentTaskModal.tsx
@@ -53,8 +53,8 @@ function EditCurrentTaskModal({
   }
 
   return (
-    <div className="fixed min-w-screen w-screen min-h-screen h-screen top-0 right-0 backdrop-blur-lg flex items-center justify-center z-10">
-      <form className="relative min-w-fit md:min-w-[500px] w-auto min-h-[600px] h-auto bg-white p-7 sm:p-10 rounded-lg flex flex-col gap-y-10 shadow-2xl">
+    <div className="fixed min-w-screen w-screen min-h-screen h-screen top-0 right-0 backdrop-blur-lg flex items-center justify-center z-10 px-4 sm:px-10 md:px-10">
+      <form className="relative min-w-full md:min-w-[500px] w-auto min-h-[600px] h-auto bg-white p-7 sm:p-10 rounded-lg flex flex-col gap-y-10 shadow-2xl">
         <IoMdClose
           className="absolute self-end top-[25px] right-[30px] cursor-pointer"
           size="18px"
